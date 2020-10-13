9657faed76893b653dd69759f2c0fdc3e499f011
Davi Santos <davi-rocha2@hotmail.com>
2020-10-12 23:56:29 -0300

    project created. Added landing and map pages

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import mapMarker from '../images/map-marker.svg';
+
+import 'leaflet/dist/leaflet.css'
+
+import '../styles/pages/orphanage.map.css';
+
+function OrphanagesMap () {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarker} alt="Happy"></img>
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando suas visita</p>
+                </header>
+
+                <footer>
+                    <strong>Ceará</strong>
+                    <span>Sobral</span>
+                </footer>
+            </aside>
+            
+            <Map
+            center={[-3.6915557,-40.3567137]}
+            zoom={15}
+            style={{ width: '100%', height: '100%'
+            }}>
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
+            
+
+            <Link className="create-orphanage" to="">
+                <FiPlus size={32} color="#FFF" />                
+            </Link>
+
+        </div>
+    )
+}
+
+export default OrphanagesMap;
+
+// mapbox - another tool to work with maps free
